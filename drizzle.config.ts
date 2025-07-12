f24933023af0001ba6977db09c92e59097400750
ortensutanto <ortensutanto@gmail.com>
2025-07-12 07:46:20 +0700

    Postgres setup

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+import { readConfig } from "./src/config.js";
+
+const config = readConfig();
+
+export default defineConfig({
+  schema: "src/schema.ts",
+  out: "src/lib/db",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: config.dbUrl,
+  },
+}); 
