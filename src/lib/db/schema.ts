b8774c047b16e63e738349b5c81d49fb8bea1bfd
ortensutanto <ortensutanto@gmail.com>
2025-07-13 12:20:03 +0700

    Created Feed DB

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -6,3 +6,13 @@ export const users = pgTable("users", {
     updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(() => new Date()),
     name: text("name").notNull().unique(),
 });
+
+export const feeds = pgTable("feeds", {
+    id: uuid("id").primaryKey().defaultRandom().notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(() => new Date()),
+    name: text("name").notNull().unique(),
+    url: text("url").notNull().unique(),
+    userId: uuid("userId").references(() => users.id, {onDelete: 'cascade'}).notNull(),
+});
+
